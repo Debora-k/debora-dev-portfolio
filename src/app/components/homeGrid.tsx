ba101b90dda8b240c2c9cfd75da41e6bf8c54078
Debora-k <debora0104k@gmail.com>
2025-01-20 16:23:01 -0700

    added spans

--- src/app/components/homeGrid.tsx
+++ src/app/components/homeGrid.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 export default function HomeGrid() {
     return (
     <div className="grid grid-cols-3 gap-4 min-h-screen p-8 pb-20 sm:p-20 bg-white font-[family-name:var(--font-geist-sans)]">
-        <Link href="/projects" className="border-solid border-2 border-black text-black">Projects</Link>
-        <Link href="/education" className="border-solid border-2 border-black text-black">Education</Link>
-        <Link href="/aboutme" className="border-solid border-2 border-black text-black">Debora Kwon</Link>
-        <Link href="/experience" className="border-solid border-2 border-black text-black">Work Experience</Link>
-        <div className="border-solid border-2 border-black text-black">Resume</div>
+        <Link href="/projects" className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Projects</span></Link>
+        <Link href="/education" className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Education</span></Link>
+        <Link href="/aboutme" className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Debora Kwon</span></Link>
+        <Link href="/experience" className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Work Experience</span></Link>
+        <div className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Resume</span></div>
         <div className="border-solid border-2 border-black text-black">Contact Me</div>
-        <Link href="/goals" className="border-solid border-2 border-black text-black">Goals</Link>
+        <Link href="/goals" className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Goals</span></Link>
     </div>
     )
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,11 @@
 export default function Projects() {
     return (
         <div className="grid grid-cols-3 gap-4 min-h-screen p-8 pb-20 sm:p-20 bg-white font-[family-name:var(--font-geist-sans)]">
-            <div className="border-solid border-2 border-black text-black">Lowdown</div>
-            <div className="border-solid border-2 border-black text-black">Pasta Maker</div>
-            <div className="border-solid border-2 border-black text-black">Kinchi Run</div>
-            <div className="border-solid border-2 border-black text-black">H&M clone</div>
-            <div className="border-solid border-2 border-black text-black">Management system</div>
+            <div className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Lowdown</span></div>
+            <div className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Pasta Maker</span></div>
+            <div className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Kinchi Run</span></div>
+            <div className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">H&M clone</span></div>
+            <div className="border-solid border-2 border-black text-black"><span className="box-decoration-clone bg-indigo-500 border-solid border-2 border-black">Management system</span></div>
         </div>
     )
 }
