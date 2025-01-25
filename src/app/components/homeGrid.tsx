697e18dd54322d227e0260a1668e70436204851f
Debora-k <debora0104k@gmail.com>
2025-01-25 00:12:49 -0700

    fixed github history about theme

--- src/app/components/homeGrid.tsx
+++ src/app/components/homeGrid.tsx
@@ -1,10 +1,14 @@
+"use client";
 // import { animated } from "@react-spring/web";
 import Link from "next/link";
 import ShootingStarBorder from "./shootingStartBorder";
 import { ModeToggle } from "./modeToggle";
 import GitHubCalendar from "react-github-calendar";
+import { useTheme } from "next-themes";
 
 export default function HomeGrid() {
+    const { theme } = useTheme();
+
     return (
         <div className="grid grid-cols-4 gap-4 min-h-screen p-8 pb-20 sm:p-20 bg-white dark:bg-black font-[family-name:var(--font-geist-sans)]">
             <Link
@@ -40,7 +44,14 @@ export default function HomeGrid() {
             </Link>
             <div className="rounded-xl border-none border border-black text-black col-start-1 row-span-4 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
                 <span className="font-lilitaOne text-2xl">Github</span>
-                <GitHubCalendar username="debora-k" />
+                <GitHubCalendar
+                    username="debora-k"
+                    colorScheme={
+                        theme === "light" || theme === "dark"
+                            ? theme
+                            : undefined
+                    }
+                />
             </div>
             <div className="rounded-xl border-none border-2 border-black text-black col-span-1 row-span-4 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
                 <span className="font-lilitaOne text-2xl">Contact</span>
