e445b325722df997e3dac4af10d2428ce793d869
Debora-k <debora0104k@gmail.com>
2025-01-28 21:44:37 -0700

    worked on grid and working on google map for location

--- src/app/components/homeGrid.tsx
+++ src/app/components/homeGrid.tsx
@@ -5,15 +5,16 @@ import ShootingStarBorder from "./shootingStartBorder";
 import { ModeToggle } from "./modeToggle";
 import GitHubCalendar from "react-github-calendar";
 import { useTheme } from "next-themes";
+import MyMapComponent from "./googleMaps";
 
 export default function HomeGrid() {
     const { theme } = useTheme();
 
     return (
-        <div className="grid grid-cols-4 grid-rows-4 grid-flow-row-dense gap-4 min-h-screen pt-10 p-4 pb-20 sm:p-10 bg-white dark:bg-black font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-cols-4 grid-rows-4 grid-flow-row-dense gap-4 min-h-screen p-4 pb-20 sm:p-10 bg-white dark:bg-black font-[family-name:var(--font-geist-sans)]">
             <Link
                 href="/aboutme"
-                className="rounded-xl border-none border-2 border-black text-black row-start-1 row-end-13 row-span-3 col-span-1 hover:bg-indigo-400 dark:border-white dark:border-solid dark:text-white"
+                className="pt-2 rounded-xl border-none border-2 border-black text-black row-start-1 row-end-13 row-span-3 col-span-1 hover:bg-indigo-400 dark:border-white dark:border-solid dark:text-white"
             >
                 <span className="font-lilitaOne text-2xl pl-3">
                     Debora Kwon
@@ -21,35 +22,36 @@ export default function HomeGrid() {
             </Link>
             <Link
                 href="/education"
-                className="rounded-xl border-none border-2 border-black text-black row-span-3 col-span-1 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white"
+                className="pt-2 rounded-xl border-none border-2 border-black text-black row-span-2 col-span-1 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white"
             >
                 <span className="font-lilitaOne text-2xl pl-3">Resume</span>
             </Link>
-            <div className="rounded-xl border-none border-2 border-black text-black row-span-3 col-span-1 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
+            <div className="pt-2 rounded-xl border-none border-2 border-black text-black row-span-2 col-span-1 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
                 <span className="font-lilitaOne text-2xl pl-3">Location</span>
+                <MyMapComponent />
             </div>
-            <span className="p-2">
+            <span className="justify-self-end col-start-5">
                 <ModeToggle />
             </span>
             <Link
                 href="/experience"
-                className="rounded-xl border-none border-2 border-black text-black row-span-6 col-span-1 col-start-4 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white"
+                className="pt-2 rounded-xl border-none border-2 border-black text-black row-span-6 col-span-1 col-start-4 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white"
             >
                 <span className="font-lilitaOne text-2xl pl-3">Education</span>
             </Link>
             <Link
                 href="/goals"
-                className="rounded-xl border-none border col-start-4 border-black text-black row-span-8 hover:bg-indigo-400 dark:border-white dark:border-solid dark:text-white"
+                className="pt-2 rounded-xl border-none border col-start-4 border-black text-black row-span-8 hover:bg-indigo-400 dark:border-white dark:border-solid dark:text-white"
             >
                 <span className="font-lilitaOne text-2xl pl-3">Experience</span>
             </Link>
             <Link
                 href="/projects"
-                className="rounded-xl border-none border-2 border-black text-black row-span-10 col-span-2 row-start-4 col-start-2 hover:bg-indigo-400 dark:border-white dark:border-solid dark:text-white"
+                className="pt-2 rounded-xl border-none border-2 border-black text-black row-span-10 col-span-2 row-start-3 col-start-2 hover:bg-indigo-400 dark:border-white dark:border-solid dark:text-white"
             >
                 <span className="font-lilitaOne text-2xl pl-3">Projects</span>
             </Link>
-            <div className="rounded-xl border-none border border-black text-black col-start-1 row-start-14 col-span-3 row-span-4 pl-24 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
+            <div className="rounded-xl border-none border border-black text-black col-start-1 row-start-14 col-span-3 row-span-7 pl-24 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
                 <span className="font-lilitaOne text-2xl pl-5">Github</span>
                 <GitHubCalendar
                     username="debora-k"
@@ -60,7 +62,7 @@ export default function HomeGrid() {
                     }
                 />
             </div>
-            <div className="rounded-xl border-none border-2 border-black text-black col-span-1 row-span-4 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
+            <div className="pt-2 rounded-xl border-none border-2 border-black text-black col-span-1 row-span-5 col-start-4 hover:bg-amber-100 dark:border-white dark:border-solid dark:text-white">
                 <span className="font-lilitaOne text-2xl pl-3">Contact</span>
             </div>
         </div>
