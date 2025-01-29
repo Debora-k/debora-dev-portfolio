dae07631e8c7ecbb42cc7d4486d240884d4f11c2
Debora-k <debora0104k@gmail.com>
2025-01-28 23:04:34 -0700

    cleaned up

--- src/app/components/googleMaps.tsx
+++ src/app/components/googleMaps.tsx
@@ -3,8 +3,8 @@ import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import "dotenv/config";
 
 const containerStyle = {
-    width: "400px",
-    height: "400px",
+    width: "250px",
+    height: "250px",
 };
 
 const center = {
@@ -18,27 +18,8 @@ function MyComponent() {
         googleMapsApiKey: process.env.MY_GOOGLE_MAPS_API,
     });
 
-    const [map, setMap] = React.useState(null);
-
-    const onLoad = React.useCallback(function callback(map) {
-        // const bounds = new window.google.maps.LatLngBounds(center);
-        // map.fitBounds(bounds);
-
-        setMap(map);
-    }, []);
-
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null);
-    }, []);
-
     return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-        >
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
             {/* Child components, such as markers, info windows, etc. */}
             <></>
         </GoogleMap>
